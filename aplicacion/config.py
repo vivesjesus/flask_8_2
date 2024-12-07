bd6ca2cb53c65dc314242f03eefc200d515b833b
vivesjesus <vivesjesus@gmail.com>
2024-12-07 13:42:29 +0100

    aa

--- aplicacion/config.py
+++ aplicacion/config.py
@@ -6,7 +6,8 @@ PWD = os.path.abspath(os.curdir)
 DEBUG = True
 # SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
 # SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://root:ORupxs59KTNbCWprN9YbIaCIRAlBiGi2@dpg-cta3frogph6c73ejjog0-a.oregon-postgres.render.com/test_93i0'
+#Externo
+#SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://root:ORupxs59KTNbCWprN9YbIaCIRAlBiGi2@dpg-cta3frogph6c73ejjog0-a.oregon-postgres.render.com/test_93i0'
 #interna
-#SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://root:ORupxs59KTNbCWprN9YbIaCIRAlBiGi2@dpg-cta3frogph6c73ejjog0-a/test_93i0'
+SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://root:ORupxs59KTNbCWprN9YbIaCIRAlBiGi2@dpg-cta3frogph6c73ejjog0-a/test_93i0'
 SQLALCHEMY_TRACK_MODIFICATIONS=False
