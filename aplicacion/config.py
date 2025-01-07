344d02435660e8a30ba7bfa25b993bcbf87b7e71
vivesjesus <59367576+vivesjesus@users.noreply.github.com>
2025-01-07 13:44:13 +0100

    Update config.py

--- aplicacion/config.py
+++ aplicacion/config.py
@@ -9,5 +9,6 @@ DEBUG = True
 #Externo
 #SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://root:ORupxs59KTNbCWprN9YbIaCIRAlBiGi2@dpg-cta3frogph6c73ejjog0-a.oregon-postgres.render.com/test_93i0'
 #interna
-SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://root:ORupxs59KTNbCWprN9YbIaCIRAlBiGi2@dpg-cta3frogph6c73ejjog0-a/test_93i0'
+#postgresql://test_m4mc_user:7Ts2lHx5HdsW2dpiEfIEKy33MlTQqezw@dpg-ctui1gbtq21c73bio7sg-a/test_m4mc
+SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://test_m4mc_user:7Ts2lHx5HdsW2dpiEfIEKy33MlTQqezw@dpg-ctui1gbtq21c73bio7sg-a/test_m4mc'
 SQLALCHEMY_TRACK_MODIFICATIONS=False
